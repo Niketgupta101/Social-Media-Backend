b9e551c2444bc57dc35b229c3960acb32b3fbc29
Niketgupta101 <72216835+Niketgupta101@users.noreply.github.com>
2022-01-02 16:58:42 +0530

    Updated userRouter with comments

--- user-management-server/src/api/routes/v1/user.route.js
+++ user-management-server/src/api/routes/v1/user.route.js
@@ -4,15 +4,19 @@ const { protect } = require('../../middlewares/auth');
 
 const router = express.Router();
 
-
+// To verify Email-Id Id.
 router.get('/verifyEmail/:verifyToken', verifyEmail);
 
+// To fetch user with given id(as param) with restrictions.
 router.get('/fetchUser/:id', protect, fetchUser);
 
+// To edit personal details of the user with given id.
 router.put('/editUser/:id', protect, editUser);
 
+// To fetch all the users with restriction( only username, id, Name)
 router.get('/fetchAllUsers', protect, getAllUsers);
 
+// to fetch user with given info like name loaction etc.
 router.get('/searchUsersWithInfo/:info', protect, searchUsers);
 
 
